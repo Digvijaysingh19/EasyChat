ee2bf7156ed9cbdccbd96b8fdc4604bbff80fb27
Digvijaysingh19 <51398185+Digvijaysingh19@users.noreply.github.com>
2019-06-23 22:32:03 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module("myList",[]);
-myApp.controller("myListController", function ($scope) {
-   $scope.item = ["AngularJS","ReactJS","UnderscoreJS"]; 
-   $scope.newitem = "" ;
-
-});

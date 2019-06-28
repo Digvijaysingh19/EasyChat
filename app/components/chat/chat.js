8032133cc2efd9e50d227ed87e8a0286a86b472c
Digvijaysingh19 <digvijaysinghsolanki786@gmail.com>
2019-06-28 11:47:27 +0530

    Buttons Added > Send Button > Drop Down Button

--- app/components/chat/chat.js
+++ app/components/chat/chat.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
      var aa = angular.module('a', ['ngMaterial'])
 
 /* When the user clicks on the button, 
@@ -20,51 +19,10 @@ window.onclick = function(event) {
     }
   }
 }
-app.controller('contact-list',function($scope,$http)
-{
-  $scope.first_name = null;
-  $scope.last_name = null;
-  $scope.key = null;
-  $scope.username = null;
 
-  $scope.postData = function (first_name, last_name, key, username)
-  {
-    var data = {
-      first_name : first_name,
-      last_name : last_name,
-      key : key,
-      username : username
-    };
-    getData(data)
-  }
-  getDatafromDB().then(function (response){
-    $scope.data = response.data;
-  })
-  // var contacts = {
-  //   first_name : first_name,
-  //   last_name : last_name,
-  //   key : key,
-  //   username : username
-  // };
-  function getDatafromDB() {
-    return $http.get('/handlers/chat')
-  }
-});
-
-// $http.get('/handlers/chat').then(function(response){
-//   $scope.contacts = response.contacts;
-// $scope.contacts = ["AngularJS","REactJs","Easychat"];
-// function getDatafromDB() {
-  // var pd
-// }        $http.get('/chat').then(function(response){
-                // $scope.contacts = response.data; 
-                // console.log(respones\)
-          //     })
-          // });
-=======
 app.controller('contact-list',function($scope,$http){
   $http.get('/handlers/chat').then(function(response){
     $scope.contacts = response.data; 
     })
 });
->>>>>>> 6562d182f44da79d9719bccb07f88fc86b075372
+

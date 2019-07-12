267fc9a0c52ba260718866e5096d5bf44e3dfd42
Digvijaysingh19 <digvijaysinghsolanki786@gmail.com>
2019-07-12 13:59:24 +0530

    Added List Icon and Arrow Button

--- app/static/app.js
+++ app/static/app.js
@@ -7,16 +7,49 @@ var app = angular.module('myApp', [
   'ngAnimate', 
   'ngAria'
 ])
-.controller('AppCtrl', function ($scope, $mdSidenav) {
+.controller('AppCtrl', function ($scope, $mdSidenav,$mdDialog) {
     $scope.toggleLeft = buildToggler('left');
 
     function buildToggler(componentId) {
       return function() {
         $mdSidenav(componentId).toggle();
       };
-    }
-  });
+    };
+    $scope.openFromLeft = function() {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('Opening from the left')
+        .textContent('Closing to the right!')
+        .ariaLabel('Left to right demo')
+        .ok('Nice!')
+        // You can specify either sting with query selector
+        .openFrom('#left')
+        // or an element
+        .closeTo(angular.element(document.querySelector('#right')))
+    );
+  };
 
+  $scope.openOffscreen = function() {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('Opening from offscreen')
+        .textContent('Closing to offscreen')
+        .ariaLabel('Offscreen Demo')
+        .ok('Amazing!')
+        // Or you can specify the rect to do the transition from
+        .openFrom({
+          top: -50,
+          width: 30,
+          height: 80
+        })
+        .closeTo({
+          left: 1500
+        })
+    );
+  };
+  });
 app.config(function($locationProvider, $routeProvider) {
   // $locationProvider.hashPrefix('!');
   $routeProvider.when('/chat', {
